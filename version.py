3eaf88d568aece40cb5a4e99abb323199fe99cac
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-02-07 15:10:14 +0300

    Update version.py

--- version.py
+++ version.py
@@ -1,7 +1,4 @@
-# UTF-8
-#
-# For more details about fixed file info 'ffi' see:
-# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+
 VSVersionInfo(
   ffi=FixedFileInfo(
     # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
